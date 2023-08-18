ef305ea349f4cf751458ebff6287be216bec2526
billycougz <billycougz@gmail.com>
2023-08-17 21:33:10 -0400

    Base meta files and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,32 @@
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `TasteBuddy`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+	siteMetadata: {
+		title: `TasteBuddy`,
+		siteUrl: `https://tastebuddy.williamcougan.com`,
+		description: 'TasteBuddy is your AI companion for food & drink recommendations',
+		image: '/site-img.jpeg',
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-styled-components',
+		},
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.jpg',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-google-gtag',
+			options: {
+				trackingIds: [process.env.GA_MEASUREMENT_ID],
+			},
+		},
+	],
 };
