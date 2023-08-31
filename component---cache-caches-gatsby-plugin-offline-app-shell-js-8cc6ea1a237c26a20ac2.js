fa8b13038cb73e042e56082e2cd1754210d197e6
billycougz <billycougz@gmail.com>
2023-08-31 19:56:43 -0400

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-8cc6ea1a237c26a20ac2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktaste_buddy=self.webpackChunktaste_buddy||[]).push([[306],{1367:function(e,t,u){var n=u(4836);t.__esModule=!0,t.default=void 0;var a=n(u(7867)),r=n(u(7294)),d=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),l=d;t.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-8cc6ea1a237c26a20ac2.js.map
