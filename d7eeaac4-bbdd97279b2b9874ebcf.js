b69f44eb21a08840e1dd8cd444564e6d30c52fa2
billycougz <billycougz@gmail.com>
2023-08-31 19:26:28 -0400

    Updates

---
+++ d7eeaac4-bbdd97279b2b9874ebcf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktaste_buddy=self.webpackChunktaste_buddy||[]).push([[617],{8193:function(t,a,c){c.d(a,{kA6:function(){return e}});var u=c(4405);function e(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"}}]})(t)}}}]);
+//# sourceMappingURL=d7eeaac4-bbdd97279b2b9874ebcf.js.map
